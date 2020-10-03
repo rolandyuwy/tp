af965003f0da70ff8b06df8f1e3b95688de943ec
jonahtanjz <jonahtanjz4884@gmail.com>
2020-10-03 16:51:51 +0800

    Fix coding style issue

--- src/main/java/seedu/address/model/person/Priority.java
+++ src/main/java/seedu/address/model/person/Priority.java
@@ -1,7 +1,5 @@
 package seedu.address.model.person;
 
-import seedu.address.model.person.exceptions.DuplicatePersonException;
-
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
