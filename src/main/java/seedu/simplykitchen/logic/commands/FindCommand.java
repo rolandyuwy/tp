6a94a6462b1b48166c872d812a2d39b6ae0c8f91
jonahtanjz <jonahtanjz4884@gmail.com>
2020-10-18 15:32:07 +0800

    Add tokenizer for find command

--- src/main/java/seedu/simplykitchen/logic/commands/FindCommand.java
+++ src/main/java/seedu/simplykitchen/logic/commands/FindCommand.java
@@ -5,6 +5,8 @@ import static java.util.Objects.requireNonNull;
 import seedu.simplykitchen.commons.core.Messages;
 import seedu.simplykitchen.model.Model;
 import seedu.simplykitchen.model.food.DescriptionContainsKeywordsPredicate;
+import seedu.simplykitchen.model.food.Priority;
+import seedu.simplykitchen.model.food.PrioritySearchPredicate;
 
 /**
  * Finds and lists all food items in Food inventory whose description contains any of the argument keywords.
@@ -17,16 +19,19 @@ public class FindCommand extends Command {
     public static final String MESSAGE_USAGE = "Usage : " + COMMAND_WORD + " KEYWORD [MORE_KEYWORDS]...\n  "
             + "Example: " + COMMAND_WORD + " apple bread carrot";
 
-    private final DescriptionContainsKeywordsPredicate predicate;
+    private final DescriptionContainsKeywordsPredicate descriptionPredicate;
+    private final PrioritySearchPredicate priorityPredicate;
 
-    public FindCommand(DescriptionContainsKeywordsPredicate predicate) {
-        this.predicate = predicate;
+    public FindCommand(DescriptionContainsKeywordsPredicate descriptionPredicate,
+                       PrioritySearchPredicate priorityPredicate) {
+        this.descriptionPredicate = descriptionPredicate;
+        this.priorityPredicate = priorityPredicate;
     }
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.updateFilteredFoodList(predicate);
+        model.updateFilteredFoodList(descriptionPredicate);
         return new CommandResult(
                 String.format(Messages.MESSAGE_FOODS_LISTED_OVERVIEW, model.getFilteredFoodList().size()));
     }
@@ -35,6 +40,7 @@ public class FindCommand extends Command {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof FindCommand // instanceof handles nulls
-                && predicate.equals(((FindCommand) other).predicate)); // state check
+                && descriptionPredicate.equals(((FindCommand) other).descriptionPredicate)
+                && priorityPredicate.equals(((FindCommand) other).priorityPredicate)); // state check
     }
 }
--- src/main/java/seedu/simplykitchen/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/simplykitchen/logic/parser/FindCommandParser.java
@@ -1,12 +1,17 @@
 package seedu.simplykitchen.logic.parser;
 
 import static seedu.simplykitchen.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.simplykitchen.logic.parser.CliSyntax.*;
+import static seedu.simplykitchen.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Arrays;
+import java.util.Set;
 
+import seedu.simplykitchen.logic.commands.AddCommand;
 import seedu.simplykitchen.logic.commands.FindCommand;
 import seedu.simplykitchen.logic.parser.exceptions.ParseException;
-import seedu.simplykitchen.model.food.DescriptionContainsKeywordsPredicate;
+import seedu.simplykitchen.model.food.*;
+import seedu.simplykitchen.model.tag.Tag;
 
 /**
  * Parses input arguments and creates a new FindCommand object
@@ -19,7 +24,15 @@ public class FindCommandParser implements Parser<FindCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindCommand parse(String args) throws ParseException {
-        String trimmedArgs = args.trim();
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_DESCRIPTION, PREFIX_PRIORITY, PREFIX_EXPIRY_DATE, PREFIX_TAG);
+
+        Description description = ParserUtil.parseDescription(argMultimap.getValue(PREFIX_DESCRIPTION).orElse(null));
+        Priority priority = ParserUtil.parsePriority(argMultimap.getValue(PREFIX_PRIORITY).orElse("low"));
+        ExpiryDate expiryDate = ParserUtil.parseExpiryDate(argMultimap.getValue(PREFIX_EXPIRY_DATE).orElse(null));
+        Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+
+        String trimmedArgs = description.toString().trim();
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
@@ -27,7 +40,8 @@ public class FindCommandParser implements Parser<FindCommand> {
 
         String[] descriptionKeywords = trimmedArgs.split("\\s+");
 
-        return new FindCommand(new DescriptionContainsKeywordsPredicate(Arrays.asList(descriptionKeywords)));
+        return new FindCommand(new DescriptionContainsKeywordsPredicate(Arrays.asList(descriptionKeywords)),
+                new PrioritySearchPredicate(priority.value));
     }
 
 }
