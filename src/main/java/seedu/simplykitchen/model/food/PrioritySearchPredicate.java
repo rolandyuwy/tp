bec6a3408767d683ef13e91a957a70f63e3fabf0
jonahtanjz <jonahtanjz4884@gmail.com>
2020-10-18 15:01:04 +0800

    Add PrioritySearchPredicate class

---
+++ src/main/java/seedu/simplykitchen/model/food/PrioritySearchPredicate.java
@@ -0,0 +1,27 @@
+package seedu.simplykitchen.model.food;
+
+import java.util.function.Predicate;
+
+/**
+ * Tests that a {@code Food}'s {@code Priority} matches the search given.
+ */
+public class PrioritySearchPredicate implements Predicate<Food> {
+    private final Priority.Level prioritySearch;
+
+    public PrioritySearchPredicate(Priority.Level prioritySearch) {
+        this.prioritySearch = prioritySearch;
+    }
+
+    @Override
+    public boolean test(Food food) {
+        return food.getPriority().equals(prioritySearch);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof PrioritySearchPredicate // instanceof handles nulls
+                && prioritySearch.equals(((PrioritySearchPredicate) other).prioritySearch)); // state check
+    }
+
+}
