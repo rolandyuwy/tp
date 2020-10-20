238f58237e5b52521cf82d8c1833474de1c8e0dc
jonahtanjz <jonahtanjz4884@gmail.com>
2020-10-20 14:11:34 +0800

    Rename keywords to tags in TagSearchPredicate

--- src/main/java/seedu/simplykitchen/model/tag/TagSearchPredicate.java
+++ src/main/java/seedu/simplykitchen/model/tag/TagSearchPredicate.java
@@ -10,18 +10,18 @@ import seedu.simplykitchen.model.food.Food;
  * Tests that a {@code Food}'s {@code Description} matches any of the keywords given.
  */
 public class TagSearchPredicate implements Predicate<Food> {
-    private final Set<Tag> keywords;
+    private final Set<Tag> tags;
 
-    public TagSearchPredicate(Set<Tag> keywords) {
-        this.keywords = keywords;
+    public TagSearchPredicate(Set<Tag> tags) {
+        this.tags = tags;
     }
 
     @Override
     public boolean test(Food food) {
-        return keywords.stream()
-                .anyMatch(keyword -> {
+        return tags.stream()
+                .anyMatch(tag -> {
                     for (Tag tag : food.getTags()) {
-                        if (StringUtil.containsWordIgnoreCase(tag.tagName, keyword.tagName)) {
+                        if (StringUtil.containsWordIgnoreCase(tag.tagName, tag.tagName)) {
                             return true;
                         }
                     }
@@ -33,7 +33,7 @@ public class TagSearchPredicate implements Predicate<Food> {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof TagSearchPredicate // instanceof handles nulls
-                && keywords.equals(((TagSearchPredicate) other).keywords)); // state check
+                && tags.equals(((TagSearchPredicate) other).tags)); // state check
     }
 
 }
