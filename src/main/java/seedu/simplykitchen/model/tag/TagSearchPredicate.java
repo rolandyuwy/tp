e579fcd7c6b403e2902d4b3fd05c96b7b2dd6676
jonahtanjz <jonahtanjz4884@gmail.com>
2020-10-20 14:14:33 +0800

    Rename tags to tagsToFind and add assertion in TagSearchPredicate

--- src/main/java/seedu/simplykitchen/model/tag/TagSearchPredicate.java
+++ src/main/java/seedu/simplykitchen/model/tag/TagSearchPredicate.java
@@ -10,18 +10,19 @@ import seedu.simplykitchen.model.food.Food;
  * Tests that a {@code Food}'s {@code Description} matches any of the keywords given.
  */
 public class TagSearchPredicate implements Predicate<Food> {
-    private final Set<Tag> tags;
+    private final Set<Tag> tagsToFind;
 
-    public TagSearchPredicate(Set<Tag> tags) {
-        this.tags = tags;
+    public TagSearchPredicate(Set<Tag> tagsToFind) {
+        assert tagsToFind != null : "Tags to find cannot be null.";
+        this.tagsToFind = tagsToFind;
     }
 
     @Override
     public boolean test(Food food) {
-        return tags.stream()
-                .anyMatch(tag -> {
+        return tagsToFind.stream()
+                .anyMatch(tagToFind -> {
                     for (Tag tag : food.getTags()) {
-                        if (StringUtil.containsWordIgnoreCase(tag.tagName, tag.tagName)) {
+                        if (StringUtil.containsWordIgnoreCase(tag.tagName, tagToFind.tagName)) {
                             return true;
                         }
                     }
@@ -33,7 +34,7 @@ public class TagSearchPredicate implements Predicate<Food> {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof TagSearchPredicate // instanceof handles nulls
-                && tags.equals(((TagSearchPredicate) other).tags)); // state check
+                && tagsToFind.equals(((TagSearchPredicate) other).tagsToFind)); // state check
     }
 
 }
