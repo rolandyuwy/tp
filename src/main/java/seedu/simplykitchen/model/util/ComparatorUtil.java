4474c176af42fc8382d3f47d1e8d34df075601f3
rolandyuwy <rolandyuwenyang@hotmail.com>
2020-10-21 19:29:28 +0800

    Update comparators for sorting

--- src/main/java/seedu/simplykitchen/model/util/ComparatorUtil.java
+++ src/main/java/seedu/simplykitchen/model/util/ComparatorUtil.java
@@ -29,15 +29,15 @@ public class ComparatorUtil {
                 food1.getPriority().isHigherPriority(food2.getPriority())
                     ? food1.getPriority().equals(food2.getPriority())
                         ? 0
-                        : -1
-                    : 1;
+                        : 1
+                    : -1;
     public static final Comparator<Food> SORT_BY_DESCENDING_PRIORITY = ( //v1.3
             food1, food2) ->
                 food1.getPriority().isHigherPriority(food2.getPriority())
                     ? food1.getPriority().equals(food2.getPriority())
                         ? 0
-                        : 1
-                    : -1;
+                        : -1
+                    : 1;
     public static final Comparator<Food> SORT_BY_DESCENDING_DESCRIPTION = (
             food1, food2) -> food2.getDescription().fullDescription.compareTo(food1.getDescription().fullDescription);
     public static final Comparator<Food> SORT_BY_ASCENDING_DESCRIPTION = ( //v1.3
